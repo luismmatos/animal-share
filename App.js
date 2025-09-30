84bb7e13f52939f21466b33d3ef62f32b41619ac
luismmatos <luismatos@example.com>
2025-09-30 15:23:23 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,7 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-
 import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <HomeScreen />
-    </View>
+    <HomeScreen />
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
